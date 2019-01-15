3998682202f6618978eb4bfab15ae9d3081ceed5
Viktor Balabushko <ViktorBalabushko@coherentsolutions.com>
2019-01-15 17:50:11 +0300

    init q

--- src/test/java/fabric/Constant.java
+++
@@ -1,9 +0,0 @@
-package fabric;
-
-public class Constant {
-    public static final String NAME_CART = "test-cartExpected";
-    public static final String FILE_PATH = "src/main/resources/";
-    public static final String FILE_NAME = FILE_PATH.concat(NAME_CART + ".json");
-    public static final double EXPECTED_WEIGHT = 159.7;
-    public static final double SIZE_ON_DISK_EXPECTED = 456.3;
-}
